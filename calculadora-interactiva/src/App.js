495db263e298bb5dcfc52354e4843eff59280156
DSW <elmcwl@gmail.com>
2023-06-15 15:37:05 -0700

    calculadora completado

--- calculadora-interactiva/src/App.js
+++ calculadora-interactiva/src/App.js
@@ -25,12 +25,12 @@ function App() {
     
   return (
     <div className='App'>
-      <div className='freecodecamp-logo-contenedor'>
+      {/* <div className='freecodecamp-logo-contenedor'>
         <img 
           src={freeCodeCampLogo}
           className='freecodecamp-logo'
           alt='Logo de freeCodeCamp' />
-      </div>
+      </div> */}
       <div className='contenedor-calculadora'>
         <Pantalla input={input} />
         <div className='fila'>
--- calculadora-interactiva/src/componentes/Boton.jsx
+++ calculadora-interactiva/src/componentes/Boton.jsx
@@ -3,13 +3,30 @@ import '../hojas-de-estilo/Boton.css';
 
 export function Boton(props) {
   const esOperador = valor => {
-    return isNaN(valor) && (valor != '.') && (valor != '=');
+    return isNaN(valor) && (valor !== '.') && (valor !== '=');
   };
 
+  // En react tambien se puede hacer que se muestre una seccion u otra de contenido segun un condicional
+  // if(esOperador(props.children)) {
+  //   return (
+  //     <div className='boton-contenedor operador'
+  //     onClick={() => props.manejarClic(props.children)}>
+  //       {props.children}
+  //     </div>
+  //   );
+  // } else {
+  //   return (
+  //     <div className='boton-contenedor'
+  //     onClick={() => props.manejarClic(props.children)}>
+  //       {props.children}
+  //     </div>
+  //   );
+  // }
+
   return (
-    <div className={`boton-contenedor ${esOperador(props.children) ? 'operador' : '' }`.trimEnd()}
+    <button className={`boton-contenedor ${esOperador(props.children) ? 'operador' : '' }`.trimEnd()}
     onClick={() => props.manejarClic(props.children)}>
       {props.children}
-    </div>
+    </button>
   );
 }
